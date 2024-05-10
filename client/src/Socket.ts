d32acc3902fe4f2b96d79240eba03ea48f9ca614
developer_deepak <developer.deepak25@gmail.com>
2024-05-10 16:20:08 +0530

    fixed TS errors

--- client/src/Socket.ts
+++ client/src/Socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // Replace with your server URL
+const socket = io(import.meta.env.VITE_API_URL); // Replace with your server URL
 
 export default socket;
